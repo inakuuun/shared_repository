7289dde48bbe43aac0057a89d18e6af5ee299777
hayashi <s.y1014torimaru@gmail.com>
2022-04-27 21:05:08 +0900

    hayashi:mamp環境構築

---
+++ hayashi/MAMP/htdocs/site01/otamesi.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>MAMPのインストールデモ</title>
+</head>
+<body style="background: orange; color: white;">
+
+
+<h1>お問い合わせ</h1>
+<form action="return.php" method="post">
+    <input type="text" name="title">
+    <input type="submit" value="送信">
+</form>
+
+</body>
+</html>
