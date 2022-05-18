6ecc755d7f37c95da92eb1fc396e0f4c8d611673
tokuda yuta <yuta.tokuda1123112309@gmail.com>
2022-05-18 12:17:48 +0900

    検証

--- tokuyuuuuuu/thanks.php
+++ tokuyuuuuuu/thanks.php
@@ -1,31 +1,31 @@
 <!DOCTYPE html>
 <html lang="jp">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>登録完了画面</title>
+  <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>登録完了画面</title>
 
-    <link rel="stylesheet" href="./css/style.css">
+      <link rel="stylesheet" href="./css/style.css">
 
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@700&family=Roboto:wght@500&display=swap" rel="stylesheet">
-    <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
-</head>
-<body>
-    <main>
-        <section class="l-thanks">
-            <div class="l-thanks__inner">
-                <h2 class="c-section__title">登録完了しました</h2>
-        <p class="p-thanks__comment">下のボタンよりログインページに移動してください。</p>
-        <a href="login.php" class="c-link c-link--signup">ログインページに移動する</a>            
-        </section>
-    </main>
+      <link rel="preconnect" href="https://fonts.googleapis.com">
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+      <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@700&family=Roboto:wght@500&display=swap" rel="stylesheet">
+      <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
+  </head>
+  <body>
+      <main>
+          <section class="l-thanks">
+              <div class="l-thanks__inner">
+                  <h2 class="c-section__title">登録完了しました</h2>
+          <p class="p-thanks__comment">下のボタンよりログインページに移動してください。</p>
+          <a href="login.php" class="c-link c-link--signup">ログインページに移動する</a>
+          </section>
+      </main>
 
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"
-    integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
-    crossorigin="anonymous"></script>
-    <script src="./js/script.js"></script>
-</body>
+      <script src="https://code.jquery.com/jquery-3.6.0.min.js"
+      integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
+      crossorigin="anonymous"></script>
+      <script src="./js/script.js"></script>
+  </body>
 </html>
